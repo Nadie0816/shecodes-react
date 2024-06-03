e5b40e186fc5d7b83dab3c0a4e18a29839e54201
Nadie0816 <64112529+Nadie0816@users.noreply.github.com>
2024-06-02 22:52:54 -0400

    updated link in footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,11 @@ import React from "react";
 export default function Footer() {
   return (
     <small>
-      <a href="/" target="_blank" class="github">
+      <a
+        href="https://github.com/Nadie0816/shecodes-react.git"
+        target="_blank"
+        class="github"
+      >
         Open-source code{" "}
       </a>
       by Briana
