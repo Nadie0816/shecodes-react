66b0120b936b32c4240dc357480c717e90a4aaa2
Nadie0816 <64112529+Nadie0816@users.noreply.github.com>
2024-06-02 23:01:42 -0400

    added to footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,6 +6,7 @@ export default function Footer() {
       <a
         href="https://github.com/Nadie0816/shecodes-react.git"
         target="_blank"
+        rel="noreferrer"
         class="github"
       >
         Open-source code{" "}
